4e602a7eb3abe0f5016e1fc150e5573296afc991
Rizkyramdani <rizkyramdani.rr05@gmail.com>
2022-01-01 12:10:53 +0700

    Menyelesaikan tugas video pertemuan 1

--- Pertemuan1/index.php
+++ Pertemuan1/index.php
@@ -2,7 +2,7 @@
 /*
 Rizky Ramdani
 193040183
-https://github.com/Rizkyramdani04/pw2022_193040183
+https://github.com/Rizkyramdani04/prakweb2021_GitHub_193040183
 Pertemuan 1 - 1 Januari 2022
 Membuat Urel GitHub
 */
